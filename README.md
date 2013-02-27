44d0e01823a6310de5f6d4393412005c541ed1f3
Dominik Zogg <dominik.zogg@gmail.com>
2013-02-27 12:30:16 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Contao LTS mysqli socket connection support
+
+### Installation
+
+#### Files
+copy the content of the src folder to your Installation (PLEASE NOT WITH APPLE FINDER), merge the data, not overwrite all
+
+#### Configuration
+
+Modify your system/config/localconfig.php
+```php
+$GLOBALS['TL_CONFIG']['dbDriver'] = 'MysqliWithSocketSupport';
+$GLOBALS['TL_CONFIG']['dbSocket'] = 'path/to/the/mysql.sock';
+```
---
+++ src/system/drivers/DB_Mysqli_Socket.php
@@ -0,0 +1,48 @@
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+
+
+class DB_MysqliWithSocketSupport extends DB_Mysqli
+{
+    /**
+     * Connect to the database server and select the database
+     */
+    protected function connect()
+    {
+        if(!array_key_exists('dbSocket', $GLOBALS['TL_CONFIG']))
+        {
+            throw new ErrorException("With this driver \$GLOBALS['TL_CONFIG']['dbSocket'] must be set!");
+        }
+
+        @$this->resConnection = new mysqli(
+            $GLOBALS['TL_CONFIG']['dbHost'],
+            $GLOBALS['TL_CONFIG']['dbUser'],
+            $GLOBALS['TL_CONFIG']['dbPass'],
+            $GLOBALS['TL_CONFIG']['dbDatabase'],
+            $GLOBALS['TL_CONFIG']['dbPort'],
+            $GLOBALS['TL_CONFIG']['dbSocket']
+        );
+        @$this->resConnection->set_charset($GLOBALS['TL_CONFIG']['dbCharset']);
+    }
+
+    /**
+     * Change the current database
+     * @param string
+     * @return boolean
+     */
+    protected function set_database($strDatabase)
+    {
+        if(!array_key_exists('dbSocket', $GLOBALS['TL_CONFIG']))
+        {
+            throw new ErrorException("With this driver \$GLOBALS['TL_CONFIG']['dbSocket'] must be set!");
+        }
+
+        @$this->resConnection = new mysqli(
+            $GLOBALS['TL_CONFIG']['dbHost'],
+            $GLOBALS['TL_CONFIG']['dbUser'],
+            $GLOBALS['TL_CONFIG']['dbPass'],
+            $strDatabase,
+            $GLOBALS['TL_CONFIG']['dbPort'],
+            $GLOBALS['TL_CONFIG']['dbSocket']
+        );
+    }
+}
